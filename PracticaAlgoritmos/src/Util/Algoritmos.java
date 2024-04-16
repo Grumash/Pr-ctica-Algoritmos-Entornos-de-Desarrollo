09a0e0b7a69d097d3809eb62bd7810d5e59a196d
Grumash <162335328+Grumash@users.noreply.github.com>
2024-04-16 17:00:59 +0200

    Metodo es primo es creado

--- PracticaAlgoritmos/src/Util/Algoritmos.java
+++ PracticaAlgoritmos/src/Util/Algoritmos.java
@@ -1,4 +1,34 @@
 package Util;
 
 public abstract class Algoritmos {
+	
+	 public int fibonacci () {
+	        
+		 
+		 
+		 
+	        return 0;
+	    }
+
+	 public int factorial (int num) {
+
+		 	
+		 
+		 
+		 
+	        return 0;
+	    }
+	 
+	 public boolean esPrimo (int num) {
+		 boolean Esprimo;
+	        if(num/2 == 0) {
+	        	System.out.println("El número"+num+" es primo");
+	        	Esprimo=true;
+	        }else 
+	        	System.out.println("El número "+num+" no es primo");
+	        	Esprimo=false;
+	        	
+		 return Esprimo
+	    }
+	
 }
