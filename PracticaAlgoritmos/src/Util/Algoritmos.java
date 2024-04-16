2d3f4e51f9ff04edc792e29e01ae534b60e7e744
Grumash <162335328+Grumash@users.noreply.github.com>
2024-04-16 17:28:27 +0200

    Metodo Factorial creado

--- PracticaAlgoritmos/src/Util/Algoritmos.java
+++ PracticaAlgoritmos/src/Util/Algoritmos.java
@@ -1,34 +1,37 @@
 package Util;
 
 public abstract class Algoritmos {
-	
-	 public int fibonacci () {
-	        
-		 
-		 
-		 
-	        return 0;
-	    }
 
-	 public int factorial (int num) {
+	public int fibonacci() {
 
-		 	
-		 
-		 
-		 
-	        return 0;
-	    }
-	 
-	 public boolean esPrimo (int num) {
-		 boolean Esprimo;
-	        if(num/2 == 0) {
-	        	System.out.println("El número"+num+" es primo");
-	        	Esprimo=true;
-	        }else 
-	        	System.out.println("El número "+num+" no es primo");
-	        	Esprimo=false;
-	        	
-		 return Esprimo
-	    }
-	
+		return 0;
+	}
+
+	public int factorial(int num) {
+
+		int Factroial = num;
+
+		for (int i = num - 1; i > 0; i--) {
+
+			Factroial = Factroial * i;
+
+		}
+
+		return 0;
+	}
+
+
+	public static boolean esprimo (int num) {
+		 boolean eEsprimo= true;
+	     if(num/2 == 0) {
+	        System.out.println("El número"+num+" es primo");
+	        eEsprimo=true;
+	     } else {
+	        System.out.println("El número "+num+" no es primo");
+	        eEsprimo=false;
+	     }
+		 return true;
+		
+	}
+
 }
