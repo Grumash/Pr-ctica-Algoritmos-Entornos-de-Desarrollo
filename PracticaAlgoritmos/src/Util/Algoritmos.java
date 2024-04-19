b4ac7e30c400ba62931c23c4daa23c49cdf2fb54
Grumash <162335328+Grumash@users.noreply.github.com>
2024-04-19 16:47:13 +0200

    Comentar la clase Algoritmos

--- PracticaAlgoritmos/src/Util/Algoritmos.java
+++ PracticaAlgoritmos/src/Util/Algoritmos.java
@@ -2,9 +2,10 @@ package Util;
 
 public abstract class Algoritmos {
 	/**
+	 * Dado un numero nos dara cual es el numero de fibonacci de es numero
 	 * 
-	 * @param num
-	 * @return
+	 * @param num el numero dado para que se calcule
+	 * @return nos retornara el numero de fibonnaci
 	 */
 	public static int fibonacci(int num) {
 
@@ -28,8 +29,10 @@ public abstract class Algoritmos {
 
 	/**
 	 * 
-	 * @param num
-	 * @return
+	 * Dado un numero nos calculara cual es su factorial
+	 * 
+	 * @param num el numero dado para que se calcule su factorila
+	 * @return nos retornara el factorial del numero dado
 	 */
 	public static int factorial(int num) {
 
@@ -46,17 +49,19 @@ public abstract class Algoritmos {
 
 	/**
 	 * 
-	 * @param num
-	 * @return
+	 * Dado un numero nos calculara si es primo
+	 * 
+	 * @param num el numero dado para que se calcule di es primo
+	 * @return nos retornara un true o un fasle suegun si el numero es primo o no
 	 */
 	public static boolean esprimo(int num) {
 		boolean Esprimo = true;
-		if (num%2 == 0) {
+		if (num % 2 == 0) {
 			System.out.println("El número " + num + " no es primo");
 			Esprimo = false;
 		} else {
 			System.out.println("El número " + num + " es primo");
-			
+
 			Esprimo = true;
 		}
 		return Esprimo;
