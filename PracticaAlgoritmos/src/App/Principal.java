a55355a018a5ce505e4f8f1c5a99aa952110a76d
Grumash <162335328+Grumash@users.noreply.github.com>
2024-04-19 16:20:06 +0200

    Funciones anteriores coregidas

--- PracticaAlgoritmos/src/App/Principal.java
+++ PracticaAlgoritmos/src/App/Principal.java
@@ -7,37 +7,39 @@ import Util.*;
 public class Principal {
 
 	public static void main(String[] args) {
-		
-		// Creamos  un array de 5 numeros aleatorios entre 1 y 10
+
+		// Creamos un array de 5 numeros aleatorios entre 1 y 10
 
 		int[] numeros = new int[5];
 		Random ran = new Random();
 		for (int i = 0; i < numeros.length; i++) {
-		numeros[i] = ran.nextInt(10) + 1;
+			numeros[i] = ran.nextInt(10) + 1;
 		}
-		
-		/** Creamos un array que usara los numeros generados por el array anterior y nos dira su fibonacci 
-		 * usando la funcion de fibonacci creada en la clase algoritmos
-		**/
-		
+
+		/**
+		 * Creamos un array que usara los numeros generados por el array anterior y nos
+		 * dira su fibonacci usando la funcion de fibonacci creada en la clase
+		 * algoritmos
+		 **/
+		int[] numeroFibonacci = new int[numeros.length];
 		for (int i = 0; i < numeros.length; i++) {
-		    int numeroFibonacci = Algoritmos.fibonacci(numeros[i]);
+			
+			numeroFibonacci[i] = Algoritmos.fibonacci(numeros[i]);	
+			
+}
+
+		System.out.println("Factoriales del array 'numeros':");
+
+		/**
+		 * Creamos un array que usara los numeros generados por el array anterior y nos
+		 * dira el factorial usando la funcion creada en la clase algoritmos
+		 **/
+		int[] resultadoFactorial = new int[numeros.length];
+		for (int i = 0; i < numeros.length; i++) {
+			 resultadoFactorial [i]= Algoritmos.factorial(numeros[i]);
+			System.out.println(resultadoFactorial[i]+" Factoriales del array 'numeros':"+numeros[i]);
+
 		}
-		
-	    
-	    System.out.println("Factoriales del array 'numeros':");
-
-		/** Creamos un array que usara los numeros generados por el array anterior y nos dira el factorial 
-		 * usando la funcion creada en la clase algoritmos
-		**/
-	    for (int i = 0; i < numeros.length; i++) {
-	        int Factorial = Algoritmos.factorial(numeros[i]);
-	        
-		        
-		    }
-	    }
-		
-		
-
 	}
 
+}
